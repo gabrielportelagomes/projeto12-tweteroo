5c1b26dcb2f23bdbb51027ed2250536b2d184a91
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2022-11-06 21:40:00 -0300

    feat: validation if all information fields are filled in the POST requests

--- src/app.js
+++ src/app.js
@@ -8,87 +8,95 @@ app.use(json());
 const users = [
   {
     username: "bobesponja",
-    avatar: "https://i.pinimg.com/originals/a6/58/d5/a658d5378a5e497fea8c564a7a217191.png"
+    avatar:
+      "https://i.pinimg.com/originals/a6/58/d5/a658d5378a5e497fea8c564a7a217191.png",
   },
   {
     username: "patrick",
-    avatar: "https://www.rbsdirect.com.br/imagesrc/25747316.jpg?w=700"
+    avatar: "https://www.rbsdirect.com.br/imagesrc/25747316.jpg?w=700",
   },
   {
     username: "sandy",
-    avatar: "https://i.pinimg.com/originals/dc/9b/be/dc9bbe88452d20e9f630d9abc0069649.jpg"
+    avatar:
+      "https://i.pinimg.com/originals/dc/9b/be/dc9bbe88452d20e9f630d9abc0069649.jpg",
   },
   {
     username: "lulamolusco",
-    avatar: "https://i1.sndcdn.com/avatars-G3hIAVrD9pY3NkLx-JzayQw-t500x500.jpg"
+    avatar:
+      "https://i1.sndcdn.com/avatars-G3hIAVrD9pY3NkLx-JzayQw-t500x500.jpg",
   },
   {
     username: "siriguejo",
-    avatar: "https://i.kym-cdn.com/entries/icons/facebook/000/026/111/4917038d8bbd7fe362bed691690c7da4.jpg"
-  }
+    avatar:
+      "https://i.kym-cdn.com/entries/icons/facebook/000/026/111/4917038d8bbd7fe362bed691690c7da4.jpg",
+  },
 ];
 
 const tweets = [
   {
     username: "bobesponja",
-    tweet: "mensagem 1"
+    tweet: "mensagem 1",
   },
   {
     username: "bobesponja",
-    tweet: "mensagem 2"
+    tweet: "mensagem 2",
   },
   {
     username: "sandy",
-    tweet: "mensagem 3"
+    tweet: "mensagem 3",
   },
   {
     username: "lulamolusco",
-    tweet: "mensagem 4"
+    tweet: "mensagem 4",
   },
   {
     username: "patrick",
-    tweet: "mensagem 5"
+    tweet: "mensagem 5",
   },
   {
     username: "siriguejo",
-    tweet: "mensagem 6"
+    tweet: "mensagem 6",
   },
   {
     username: "patrick",
-    tweet: "mensagem 7"
+    tweet: "mensagem 7",
   },
   {
     username: "sandy",
-    tweet: "mensagem 8"
+    tweet: "mensagem 8",
   },
   {
     username: "patrick",
-    tweet: "mensagem 9"
+    tweet: "mensagem 9",
   },
   {
     username: "lulamolusco",
-    tweet: "mensagem 10"
+    tweet: "mensagem 10",
   },
   {
     username: "siriguejo",
-    tweet: "mensagem 11"
+    tweet: "mensagem 11",
   },
   {
     username: "bobesponja",
-    tweet: "mensagem 12"
+    tweet: "mensagem 12",
   },
   {
     username: "sandy",
-    tweet: "mensagem 13"
+    tweet: "mensagem 13",
   },
   {
     username: "bobesponja",
-    tweet: "mensagem 14"
-  }
+    tweet: "mensagem 14",
+  },
 ];
 
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
+
+  if (!username || !avatar) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+  }
 
   const newUser = {
     username,
@@ -101,6 +109,10 @@ app.post("/sign-up", (req, res) => {
 
 app.post("/tweets", (req, res) => {
   const { username, tweet } = req.body;
+
+  if (!username || !tweet) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+  }
 
   const newTweet = {
     username,
